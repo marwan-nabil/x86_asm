2f0b4aa345630369ea4e9c3cdf5f77495760bb2a
Marwan Nabil <marwan.nabil.mohammed@gmail.com>
2021-10-29 21:53:13 +0200

    simplified project structure

--- scripts/startup.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-subst W: /D
-subst W: %cd%
-W:
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
-set path=W:\scripts;C:\Program Files\NASM;%path%
-cls
---
+++ startup.bat
@@ -0,0 +1,4 @@
+@echo off
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+set path=W:\x86_asm;%path%
+cls
